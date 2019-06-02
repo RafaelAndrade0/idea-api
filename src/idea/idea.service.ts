e84ada7560edc5161fe76ff715ff3fa79f728b83
Rafael Andrade <rafael.asilva@dcomp.ufs.br>
2019-06-02 16:25:00 -0300

    First Commit

---
+++ src/idea/idea.service.ts
@@ -0,0 +1,41 @@
+import { Injectable, HttpException, HttpStatus } from '@nestjs/common';
+import { Repository } from 'typeorm';
+import { IdeaEntity } from './idea.entity';
+import { InjectRepository } from '@nestjs/typeorm';
+import { IdeaDTO } from './idea.dto';
+
+@Injectable()
+export class IdeaService {
+  constructor(
+    @InjectRepository(IdeaEntity)
+    private ideaRepository: Repository<IdeaEntity>,
+  ) {}
+
+  async showAll() {
+    return await this.ideaRepository.find();
+  }
+
+  async create(data: IdeaDTO) {
+    const idea = this.ideaRepository.create(data);
+    await this.ideaRepository.save(idea);
+    return idea;
+  }
+
+  async read(id: string) {
+    const idea = await this.ideaRepository.findOne({ where: { id: id } });
+    if (!idea) {
+      throw new HttpException('Not Found', HttpStatus.NOT_FOUND);
+    }
+    return idea;
+  }
+
+  async update(id: string, data: Partial<IdeaDTO>) {
+    await this.ideaRepository.update({ id }, data);
+    return await this.ideaRepository.findOne({ id });
+  }
+
+  async destroy(id: string) {
+    await this.ideaRepository.delete(id);
+    return { deleted: true };
+  }
+}
